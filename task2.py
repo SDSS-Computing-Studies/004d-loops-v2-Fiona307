ddd5300defd336b2cd8da07c18fe7c9f581e0aaa
Fiona307 <71406797+Fiona307@users.noreply.github.com>
2020-10-01 12:31:08 -0700

    Update task2.py

---
+++ task2.py
@@ -0,0 +1,9 @@
+#! python3
+
+name = input("Enter a name")
+Namelist = ("Alex","Sherry","Ben","Cathy","Joe")
+
+for i in Namelist:
+    if i ==name:
+        print("The name is on the list")
+        
