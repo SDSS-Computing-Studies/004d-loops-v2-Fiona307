6c8df33dc0363a35681c59c26ed72e396cb7dbb0
Fiona307 <71406797+Fiona307@users.noreply.github.com>
2020-10-02 11:31:35 -0700

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,6 +1,6 @@
 #! python3
 
-numList= (1,2,3,5,6,8,13,15,21,23,25,27,29,30,34,37,38,40)
-for i in numbers:
+numList = (25,8,10,11,33,30,51,75,63,14,20,99)
+for i in numList:
     if i%5 == 0:
         print(i)
